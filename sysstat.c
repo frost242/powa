204a4c1e071ffd260a4eda83a65fd5fdebf9cc71
Thomas Reiss <thomas.reiss@dalibo.com>
2014-12-17 21:05:21 +0100

    Fix pg_memusage to return result directly instead of using the SRF API.

--- sysstat.c
+++ sysstat.c
@@ -27,17 +27,6 @@
 #ifdef __linux__
 #define PROCFS "/proc"
 
-#define GET_NEXT_VALUE(p, q, value, length, msg, delim) \
-        if ((q = strchr(p, delim)) == NULL) \
-        { \
-            elog(ERROR, msg); \
-            return 0; \
-        } \
-        length = q - p; \
-        strncpy(value, p, length); \
-        value[length] = '\0'; \
-        p = q + 1;
-
 #define SKIP_TOKEN(p) \
 		/* Skipping leading white space. */ \
 		while (isspace(*p)) \
@@ -56,8 +45,6 @@ enum cputime { i_user, i_nice_c, i_system, i_idle, i_iowait, i_irq, i_softirq,
 enum memusage { i_memused, i_memfree, i_memshared, i_membuffers, i_memcached,
     i_swapused, i_swapfree, i_swapcached
 };
-
-int get_memusage(char **values);
 
 Datum       pg_cputime(PG_FUNCTION_ARGS);
 Datum       pg_loadavg(PG_FUNCTION_ARGS);
@@ -150,7 +137,7 @@ Datum pg_cputime(PG_FUNCTION_ARGS)
 
 Datum pg_loadavg(PG_FUNCTION_ARGS)
 {
-    struct statfs sb;
+    struct      statfs sb;
     int         fd;
     int         len;
     char        buffer[4096];
@@ -211,123 +198,37 @@ Datum pg_loadavg(PG_FUNCTION_ARGS)
 
 Datum pg_memusage(PG_FUNCTION_ARGS)
 {
-    FuncCallContext *funcctx;
-    int         call_cntr;
-    int         max_calls;
-    TupleDesc   tupdesc;
-    AttInMetadata *attinmeta;
+    int64       memused = 0;
+    int64       memfree = 0;
+    int64       memshared = 0;
+    int64       membuffers = 0;
+    int64       memcached = 0;
+    int64       swapused = 0;
+    int64       swapfree = 0;
+    int64       swapcached = 0;
 
+    int64       memtotal = 0;
+    int64       swaptotal = 0;
 
-    elog(DEBUG5, "pg_memusage: Entering stored function.");
+    TupleDesc   tupleDesc;
+    HeapTuple   tuple;
+    Datum       values[8];
+    bool        nulls[8];
+    Datum       result;
 
-    /*
-       stuff done only on the first call of the function 
-     */
-    if (SRF_IS_FIRSTCALL())
-      {
-          MemoryContext oldcontext;
-
-          /*
-             create a function context for cross-call persistence 
-           */
-          funcctx = SRF_FIRSTCALL_INIT();
-
-          /*
-             switch to memory context appropriate for multiple function calls 
-           */
-          oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
-
-          /*
-             Build a tuple descriptor for our result type 
-           */
-          if (get_call_result_type(fcinfo, NULL, &tupdesc) !=
-              TYPEFUNC_COMPOSITE)
-              ereport(ERROR,
-                      (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-                       errmsg("function returning record called in context "
-                              "that cannot accept type record")));
-
-          /*
-           * generate attribute metadata needed later to produce tuples from raw
-           * C strings
-           */
-          attinmeta = TupleDescGetAttInMetadata(tupdesc);
-          funcctx->attinmeta = attinmeta;
-
-          funcctx->max_calls = 1;
-
-          MemoryContextSwitchTo(oldcontext);
-      }
-
-    /*
-       stuff done on every call of the function 
-     */
-    funcctx = SRF_PERCALL_SETUP();
-
-    call_cntr = funcctx->call_cntr;
-    max_calls = funcctx->max_calls;
-    attinmeta = funcctx->attinmeta;
-
-    if (call_cntr < max_calls)  /* do when there is more left to send */
-      {
-          HeapTuple   tuple;
-          Datum       result;
-
-          char      **values = NULL;
-
-          values = (char **) palloc(8 * sizeof(char *));
-          values[i_memused] = (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_memfree] = (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_memshared] =
-              (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_membuffers] =
-              (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_memcached] =
-              (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_swapused] =
-              (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_swapfree] =
-              (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-          values[i_swapcached] =
-              (char *) palloc((BIGINT_LEN + 1) * sizeof(char));
-
-          if (get_memusage(values) == 0)
-              SRF_RETURN_DONE(funcctx);
-
-          /*
-             build a tuple 
-           */
-          tuple = BuildTupleFromCStrings(attinmeta, values);
-
-          /*
-             make the tuple into a datum 
-           */
-          result = HeapTupleGetDatum(tuple);
-
-          SRF_RETURN_NEXT(funcctx, result);
-      }
-    else                        /* do when there is no more left */
-      {
-          SRF_RETURN_DONE(funcctx);
-      }
-}
-
-int get_memusage(char **values)
-{
 #ifdef __linux__
-    int         length;
-    unsigned long memfree = 0;
-    unsigned long memtotal = 0;
-    unsigned long swapfree = 0;
-    unsigned long swaptotal = 0;
-
     struct statfs sb;
     int         fd;
     int         len;
     char        buffer[4096];
     char       *p;
-    char       *q;
+#endif                          /* __linux__ */
 
+    if (get_call_result_type(fcinfo, NULL, &tupleDesc) != TYPEFUNC_COMPOSITE)
+        elog(ERROR, "return type must be a row type");
+    Assert(tupleDesc->natts == lengthof(values));
+
+#ifdef __linux__
     /*
        Check if /proc is mounted. 
      */
@@ -351,84 +252,77 @@ int get_memusage(char **values)
 
     p = buffer - 1;
 
-    values[i_memshared][0] = '0';
-    values[i_memshared][1] = '\0';
-
     while (p != NULL)
       {
           ++p;
           if (strncmp(p, "Buffers:", 8) == 0)
             {
                 SKIP_TOKEN(p);
-                GET_NEXT_VALUE(p, q, values[i_membuffers], length,
-                               "Buffers not found", ' ');
+				membuffers = strtoul(p, &p, 10);
             }
           else if (strncmp(p, "Cached:", 7) == 0)
             {
                 SKIP_TOKEN(p);
-                GET_NEXT_VALUE(p, q, values[i_memcached], length,
-                               "Cached not found", ' ');
+				memcached = strtoul(p, &p, 10);
             }
           else if (strncmp(p, "MemFree:", 8) == 0)
             {
                 SKIP_TOKEN(p);
                 memfree = strtoul(p, &p, 10);
-                snprintf(values[i_memused], BIGINT_LEN, "%lu",
-                         memtotal - memfree);
-                snprintf(values[i_memfree], BIGINT_LEN, "%lu", memfree);
+				memused = memtotal - memfree;
             }
           else if (strncmp(p, "MemShared:", 10) == 0)
             {
+				/* does not exist anymore in kernel 3.13
+                 * TODO: remove this counter
+                 */
                 SKIP_TOKEN(p);
-                GET_NEXT_VALUE(p, q, values[i_memshared], length,
-                               "MemShared not found", ' ');
+                memshared = strtoul(p, &p, 10);
+            }
+          else if (strncmp(p, "Shmem:", 6) == 0)
+            {
+				/* TODO: Fix this part */
+                SKIP_TOKEN(p);
+                memshared = strtoul(p, &p, 10);
             }
           else if (strncmp(p, "MemTotal:", 9) == 0)
             {
                 SKIP_TOKEN(p);
                 memtotal = strtoul(p, &p, 10);
-                elog(DEBUG5, "pg_memusage: MemTotal = %lu", memtotal);
             }
           else if (strncmp(p, "SwapFree:", 9) == 0)
             {
                 SKIP_TOKEN(p);
                 swapfree = strtoul(p, &p, 10);
-                snprintf(values[i_swapused], BIGINT_LEN, "%lu",
-                         swaptotal - swapfree);
-                snprintf(values[i_swapfree], BIGINT_LEN, "%lu", swapfree);
+				swapused = swaptotal - swapfree;
             }
           else if (strncmp(p, "SwapCached:", 11) == 0)
             {
                 SKIP_TOKEN(p);
-                GET_NEXT_VALUE(p, q, values[i_swapcached], length,
-                               "SwapCached not found", ' ');
+                swapcached = strtoul(p, &p, 10);
             }
           else if (strncmp(p, "SwapTotal:", 10) == 0)
             {
                 SKIP_TOKEN(p);
                 swaptotal = strtoul(p, &p, 10);
-                elog(DEBUG5, "pg_memusage: SwapTotal = %lu", swaptotal);
             }
           p = strchr(p, '\n');
       }
 #endif                          /* __linux__ */
 
-    elog(DEBUG5, "pg_memusage: [%d] Buffers = %s", (int) i_membuffers,
-         values[i_membuffers]);
-    elog(DEBUG5, "pg_memusage: [%d] Cached = %s", (int) i_memcached,
-         values[i_memcached]);
-    elog(DEBUG5, "pg_memusage: [%d] MemFree = %s", (int) i_memfree,
-         values[i_memfree]);
-    elog(DEBUG5, "pg_memusage: [%d] MemUsed = %s", (int) i_memused,
-         values[i_memused]);
-    elog(DEBUG5, "pg_memusage: [%d] MemShared = %s", (int) i_memshared,
-         values[i_memshared]);
-    elog(DEBUG5, "pg_memusage: [%d] SwapCached = %s", (int) i_swapcached,
-         values[i_swapcached]);
-    elog(DEBUG5, "pg_memusage: [%d] SwapFree = %s", (int) i_swapfree,
-         values[i_swapfree]);
-    elog(DEBUG5, "pg_memusage: [%d] SwapUsed = %s", (int) i_swapused,
-         values[i_swapused]);
+    memset(nulls, 0, sizeof(nulls));
+    memset(values, 0, sizeof(values));
+    values[0] = Int64GetDatum(memused);
+    values[1] = Int64GetDatum(memfree);
+    values[2] = Int64GetDatum(memshared);
+    values[3] = Int64GetDatum(membuffers);
+    values[4] = Int64GetDatum(memcached);
+    values[5] = Int64GetDatum(swapused);
+    values[6] = Int64GetDatum(swapfree);
+    values[7] = Int64GetDatum(swapcached);
 
-    return 1;
+    tuple = heap_form_tuple(tupleDesc, values, nulls);
+    result = HeapTupleGetDatum(tuple);
+
+    PG_RETURN_DATUM(result);
 }
